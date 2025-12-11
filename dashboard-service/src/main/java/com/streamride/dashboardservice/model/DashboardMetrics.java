870954cdb4e55c3b133ac3772a35469603461d09
Sidharth Krishna <sidharth.krishna@email.com>
2025-12-11 13:12:18 +0530

    auto-refresh issue fix plus timestamp switched from UTC to IST for display

--- dashboard-service/src/main/java/com/streamride/dashboardservice/model/DashboardMetrics.java
+++ dashboard-service/src/main/java/com/streamride/dashboardservice/model/DashboardMetrics.java
@@ -1,12 +1,11 @@
 package com.streamride.dashboardservice.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 @Data
@@ -19,6 +18,5 @@ public class DashboardMetrics {
     private Double averageDuration;
     private List<CityMetrics> topCities;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime timestamp;
+    private Instant timestamp;
 }
